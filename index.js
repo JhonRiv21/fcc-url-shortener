82daeace538f4b4ccce95eb80a565939e8e7cac1
JhonRiv21 <jhonrivero21@gmail.com>
2025-06-07 00:52:55 -0500

    first commit

--- index.js
+++ index.js
@@ -2,16 +2,46 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+let urls = [];
+let id = 1;
+
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/shorturl', (req, res) => {
+  let url;
+  try {
+    url = new URL(req.body.url)
+  } catch (err) {
+    res.json({ error: 'invalid url' })
+  }
+  dns.lookup(url.hostname, (err) => {
+    if (err) return res.json({ error: 'invalid url' });
+
+    urls.push({ original_url: req.body.url, short_url: id });
+    res.json({ original_url: req.body.url, short_url: id });
+    id++;
+  })
+});
+
+app.get('/api/shorturl/:short', (req, res) => {
+  const entry = urls.find(u => u.short_url == req.params.short);
+  if (entry) {
+    res.redirect(entry.original_url);
+  } else {
+    res.json({ error: 'No short URL found' });
+  };
 });
 
 // Your first API endpoint
